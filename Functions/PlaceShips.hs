7bb97c433028a677ecd7c5f7c6c5aa5fe0750596
Botond Ortutay <boti.ortutay@gmail.com>
2024-04-10 17:29:37 +0300

    progress save

--- Functions/PlaceShips.hs
+++ Functions/PlaceShips.hs
@@ -4,4 +4,20 @@ import Datatypes.Board
 import Functions.GameBoardMutator
 
 placeShips :: Board -> Board -> Int -> Int -> Int -> String -> (Board,Board)
-placeShips pBoard eBoard shipsToPlace pInpX pInpY vertChoice = ((gameBoardMutator pBoard '□' pInpX pInpY),eBoard)
+placeShips pBoard eBoard shipsToPlace pInpX pInpY vertChoice = ((fst (modifyOnValidInput pBoard pInpX pInpY)),(snd (modifyOnValidInput pBoard pInpX pInpY)))
+
+-----------------------------
+-- [Logic of placing ships] --
+-----------------------------
+-- For player: check placement validity
+-- Is placement valid?
+-- -- If yes: place ship. -> Enemy ship placement
+-- -- If no: error message. Do not modify boards -> Returns
+-- Enemy ship placement: Check validity for random coord val
+-- Is it valid?
+-- -- If yes: place ship. -> Returns
+-- -- If no: -> Enemy ship placement
+-- Returns: (pBoard,eBoard)
+
+modifyOnValidInput :: Board -> Int -> Int -> (Board,Board)
+modifyOnValidInput pBoard pInpX pInpY = ((gameBoardMutator pBoard '□' pInpX pInpY),pBoard)
